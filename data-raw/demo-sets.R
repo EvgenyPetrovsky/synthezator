e9e4ca6c5f3dd9c34c8ca7008e895b1271f22822
Evgeny Petrovsky <evgeny.petrovsky@ing.nl>
2018-12-24 14:40:28 +0100

    rename demo_sets into demo-sets

--- data-raw/demo-sets.R
+++ data-raw/demo-sets.R
@@ -1,5 +1,5 @@
 demo_sets <- utils::read.csv(
-  file = "./data-raw/demo_sets.csv",
+  file = "./data-raw/demo-sets.csv",
   header = TRUE, stringsAsFactors = FALSE
 )
 devtools::use_data(demo_sets, overwrite = TRUE)
